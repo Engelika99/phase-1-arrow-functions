54fdf78c64f5894f182dab86569f86bedbe936d6
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-03-18 15:02:21 -0400

    Uses arrow function

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (parameter1, parameter2) => {
+    console.log(`Dividing ${parameter1}`);
+    console.log(`Dividing ${parameter2}`);
+    return parameter1/parameter2;
+    
+}
+divide(2000,100);
+
+let x = 2
+const square = x => x*x;
+
+let a = 3;
+let b = 4;
+const add = (a, b) => a + b;
+add(3,4);
+
+
